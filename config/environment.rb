867df2e1219936ec7dfabb47abfd09063d5c5e3b
Chris Kimpton <chris@kimptoc.net>
2011-11-04 18:49:11 +0000

    init

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Mybackbone1::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mybackbone1::Application.config.secret_token = '529de56e4c8fd46cf5bc7ddba70dc6e64eda41357b5685842e8716890c06f1f28229adae87f971d0f96118f054e58bad70878948599086556f8298d5f7f3325c'
